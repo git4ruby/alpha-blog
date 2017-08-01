15c3b74744c1372ffc2df0a084b9e39820fc8857
Mohit Chandana <chandanamohit@gmail.com>
2017-08-01 00:11:56 -0600

    Changed user id for article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = User.last
 		if @article.save
 			flash[:success] = "Article is successfully created!"
 			redirect_to article_path(@article)
